9707323a75398883921dd1c4721eb816a150f9e8
msk00007 <109849322+msk00007@users.noreply.github.com>
2023-03-19 14:39:30 +0530

    Add files via upload

---
+++ login/signincheck.php
@@ -0,0 +1,48 @@
+
+<?php
+
+$is_invalid = false;
+    
+$host = "localhost";
+$dbname = "login_db";
+$username = "root";
+$password = "";
+
+$mysqli = new mysqli(hostname: $host,
+                     username: $username,
+                     password: $password,
+                     database: $dbname);
+                     
+if ($mysqli->connect_errno) {
+    die("Connection error: " . $mysqli->connect_error);
+}
+    
+    $email = $_POST["email"];
+    $sql ="SELECT * FROM itlab
+                    WHERE email ='$email'";
+
+        
+    $result = $mysqli->query($sql);
+    
+    $itlab = $result->fetch_assoc();
+    
+    if ($itlab) {
+        
+        if ($_POST["psd"]===$itlab["password"]) {
+            session_start();
+            
+            session_regenerate_id();
+            
+            $_SESSION["user_id"] = $itlab["id"];
+            
+            header("Location:store.html");
+            exit;
+    }
+    
+   
+}
+$is_invalid = true;
+if($is_invalid){
+    echo "invalid login please try again with correct credentials..";
+}
+?>
