94c7375ec0af764eea78cdf74259681ccd791b84
msk00007 <109849322+msk00007@users.noreply.github.com>
2023-03-19 14:49:21 +0530

    Update payments.php

--- shopingcart/payments.php
+++ shopingcart/payments.php
@@ -1,16 +1,4 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "login_db";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     $servername = "localhost";
